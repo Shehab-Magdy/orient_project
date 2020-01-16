0fa063249fe72fbf8f91f315e59f0b3e3b20b1ed
Shehab.Magdy <shehab.magdy@g2k-group.com>
2020-01-16 14:18:05 +0200

    tring to fix drop down

--- user_management/models/profile.py
+++ user_management/models/profile.py
@@ -3,9 +3,6 @@ from django.contrib.auth.models import User
 from orient_advances.models import Section
 
 class Profile(models.Model):
-    """Model definition for Profile."""
-
-    # TODO: Define fields here
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.png',upload_to='profile_img')
     employee_id = models.IntegerField(unique=True)
@@ -13,11 +10,8 @@ class Profile(models.Model):
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
 
     class Meta:
-        """Meta definition for Profile."""
-
         verbose_name = 'Profile'
         verbose_name_plural = 'Profiles'
 
     def __str__(self):
-        """Unicode representation of Profile."""
         return f'{self.user.username} Profile'
--- user_management/views/register.py
+++ user_management/views/register.py
@@ -2,15 +2,25 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from user_management.forms.register_form import UserRegisterForm
 from orient_advances.models import Section
+from django.contrib.auth.models import User
 
 def register_view(request):
     sections = Section.objects.all()
     if request.POST:
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}, You can now login.')
+            newUser = User()
+            newUser.email = request.POST.get('email')
+            newUser.username = request.POST.get('username')
+            newUser.password = request.POST.get('password1')
+            newUser.Profile.fullname = request.POST.get('fullname')
+            newUser.Profile.employee_id = request.POST.get('employee_id')
+            newUser.Profile.section = request.POST.get('section')
+            newUser.Profile.is_boss = True if request.POST.get('section-dd') else False
+
+            messages.success(request, f'Account created for {newUser.username}, You can now login.')
+            print('success')
+            # newUser.save()
             return redirect('login')
     else:
         form = UserRegisterForm()

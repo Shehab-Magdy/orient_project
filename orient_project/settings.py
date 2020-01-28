c79cc37dd8888eaac13dcf51871b9e706469c826
Shehab.Magdy <shehab.magdy@g2k-group.com>
2020-01-28 13:45:30 +0200

    some settings for deploy

--- orient_project/settings.py
+++ orient_project/settings.py
@@ -125,6 +125,7 @@ DATE_INPUT_FORMATS = ['%d-%m-%Y']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')

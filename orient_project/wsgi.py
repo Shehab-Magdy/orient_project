81764e7b8edc7dee0e1be8783a7a5df4c9f10ca7
Shehab Magdy El-Adl <cegres1@hotmail.com>
2020-02-06 20:52:38 +0200

    adding section as string in account model

--- orient_project/wsgi.py
+++ orient_project/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orient_project.settings')
 # os.environ["DJANGO_SETTINGS_MODULE"] = "orient_project.settings"
